e77ccd442675862dceb1601f6cfdfc04528a5cd6
onlinedata8888 <onlinedata8888@gmail.com>
2026-09-19 12:05:08 +0530

    Fix packaging block indentation in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,10 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
-    packaging {
+        packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/versions/9/OSGI-INF/MANIFEST.MF"
         }
     }
 }
